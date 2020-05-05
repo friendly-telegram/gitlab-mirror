1bab6d830602cf92d5dd280e6ffb8f05e6163bb0
Hackintosh 5 <hackintosh5@wildfyre.net>
2020-05-05 14:40:44 +0100

    fix lint

--- friendly-telegram/main.py
+++ friendly-telegram/main.py
@@ -292,7 +292,8 @@ def main():  # noqa: C901
     loops = [amain(client, clients, web, arguments) for client in clients]
 
     loop.set_exception_handler(lambda _, x:
-                               logging.error("Exception on event loop! %s", x["message"], exc_info=x.get("exception", None)))
+                               logging.error("Exception on event loop! %s", x["message"],
+                                             exc_info=x.get("exception", None)))
     loop.run_until_complete(asyncio.gather(*loops))
 
 
